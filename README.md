d926a4bea6bce116d4262716b4d0ff34a39a4215
strobecat <203321455+strobecat@users.noreply.github.com>
2025-08-16 21:09:31 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# fix\_packageinstaller
+
+米兔通过改代码的暴力手段把PackageInstaller弄坏了，导致我们不能直接安装apk，我们给他修好  
+dex来自米兔3c odex
---
+++ customize.sh
@@ -0,0 +1,31 @@
+SKIPUNZIP=1
+
+if [ "$API" != '19' ] || [ "$ARCH" != 'arm' ]; then
+  abort '! arm32 kitkat expected'
+fi
+
+mkdir -p "$MODPATH/system/app"
+
+output="$MODPATH/system/app/PackageInstaller.odex"
+maxfd=$(ulimit -n 2>/dev/null) || maxfd=256
+outputfd=3
+
+while :; do
+    # test if fd is already open
+    if [ ! -e "/proc/self/$outputfd" ]; then
+        eval "exec $outputfd<>$output"
+        if [ "$?" -eq 0 ]; then
+	    break
+        fi
+    fi
+    outputfd=$((outputfd + 1))
+    if [ "$outputfd" -gt "$maxfd" ]; then
+        abort "! File descriptors run out"
+    fi
+done
+
+# https://android.googlesource.com/platform/frameworks/native/+/refs/tags/android-4.4.4_r2.0.1/cmds/installd/commands.c#581
+/system/bin/dexopt --zip 0 "$outputfd" "$ZIPFILE" "$(getprop dalvik.vm.dexopt-flags)" < "$ZIPFILE" || abort '! Failed to dexopt' 
+
+extract "$ZIPFILE" 'module.prop' "$MODPATH"
+set_perm_recursive "$MODPATH" 0 0 0755 0644
